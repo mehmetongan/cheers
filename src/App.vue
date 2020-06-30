89e6898438a49a9ffbeea7e1670e5c63b2766eef
Mehmet Ongan <mehmet@raccoon.ch>
2020-06-30 21:16:59 +0200

    add new component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,93 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-cog</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Cheers</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col class="">
+           <Beers/>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  
+    <v-footer app>
+    </v-footer>
+    <v-bottom-navigation
+      :value="activeBtn"
+      color="primary"
+      horizontal
+    >
+      <v-btn>
+        <span>Recents</span>
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Favorites</span>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Nearby</span>
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+//import HelloWorld from './components/HelloWorld';
+import Beers from './components/Beers';
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'App',
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  components: {
+    //HelloWorld,
+    Beers
+  },
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Beers.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <v-row class="beers">
+      <v-col v-for="beer in beers" :key="beer.data.id" cols="3">
+          test
+        <!-- <img :src="beer.data.labels.medium"> -->
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+  export default {
+    name: 'Beers',
+    data: () => ({
+      beers: []
+    }),
+    mounted() {
+        this.loadBeers();
+    },
+    methods: {
+        loadBeers() {
+            axios
+            .get('https://sandbox-api.brewerydb.com/v2/beers/?key=6b4c6ac74f967e90036017a778942f57',
+               { 
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Transfer-Encoding': 'cunked',
+                        'Connection': 'keep-alive'
+                    }
+               })
+            .then(response => {
+                this.beers = response.data;
+                console.log('reponse : ' + response.data);
+            })
+            .catch(error => {
+                console.log('erreur :' + error);
+            })
+        }
+    }
+  }
+</script>
